eb3dae8ee7a74cac035bfe136681071c2b2235fb
jrh229 <126018610+jrh229@users.noreply.github.com>
2023-10-21 13:36:52 -0400

    Add files via upload

--- AutoComplete.java
+++ AutoComplete.java
@@ -227,7 +227,8 @@ public class AutoComplete implements AutoCompleteInterface {
    * in the dictionary and false otherwise
    */
     public boolean advance(char c){
-      System.out.println("Curr Prefix" + currentPrefix);
+      System.out.println("Curr Prefix " + currentPrefix);
+      System.out.println("Current Node "+ currentNode.data);
       boolean foundit = false;
       if(currentPrefix.length()==0){                                            //If we are starting from root
         currentNode = root;
